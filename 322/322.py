5374cd923bbd6e00a41c086bfe127f71b18640e9
Linxiang Hu <u7633783@anu.edu.au>
2024-03-24 16:21:23 +1100

    322 finished

---
+++ 322/322.py
@@ -0,0 +1,27 @@
+import sys
+
+'''使用动态规划来解题'''
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # 初始化dp, dp[0]肯定
+        dp = [0] * (amount + 1)
+        # 对金额进行遍历
+        for i in range(1, amount + 1):
+            min_value = sys.maxsize
+            # 对硬币的面值进行遍历
+            for coin in coins:
+                # i-coin >=0 表示加上一个硬币
+                # dp[i-coin]<min_value表示有更优解
+                if i - coin >= 0 and dp[i - coin] < min_value:
+                    min_value = dp[i - coin] + 1
+                dp[i] = min_value
+
+        return dp[amount] if dp[amount] != sys.maxsize else -1
+
+
+coins = [2]
+amount = 3
+solution = Solution()
+print(solution.coinChange(coins, amount))
