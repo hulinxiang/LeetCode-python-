c5bfc1117a3bbcebbdec5b86b31efa7936c7b088
Linxiang Hu <u7633783@anu.edu.au>
2024-05-09 22:53:16 +1000

    2192 finished

---
+++ 2192/2192.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+        g = [[] for _ in range(n)]
+        # 反向构建邻接表
+        for e in edges:
+            g[e[1]].append(e[0])
+
+        def dfs(x: int) -> None:
+            visit[x] = True  # 代表访问了这个节点
+            for y in g[x]:
+                visit[y] = True
+                dfs(y)
+
+        ans = [None] * n
+        for i in range(n):
+            visit = [False] * n
+            dfs(i)
+            visit[i] = False
+            ans[i] = [j for j, b in enumerate(visit) if b]
+
+        return ans
